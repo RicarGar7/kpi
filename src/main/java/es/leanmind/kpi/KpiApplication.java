89c1f8f919d57edd6c71cef4309fd2d5dac23d9d
Ricardo García <ricartfe@gmail.com>
2022-10-07 08:48:06 +0100

    :tada: Begin project

---
+++ src/main/java/es/leanmind/kpi/KpiApplication.java
@@ -0,0 +1,13 @@
+package es.leanmind.kpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiApplication.class, args);
+    }
+
+}
